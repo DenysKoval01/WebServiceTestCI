e76fa2f2cca89f8691f5a52e55836db0c9040c92
Oleksandra <Oleksandra@LAPTOP-JISKA42N>
2023-06-02 13:21:32 +0300

    Добавьте файлы проекта.

---
+++ WebServiceTestCI/TestData/TestBookingModels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using WebServicesCI.DTO;
+
+namespace WebServicesCI.TestData
+{
+    public static class TestBookingModels
+    {
+        public static BookingModel ValidBookingModel = new BookingModel()
+        {
+            FirstName = "Jim",
+            LastName = "Brown",
+            TotalPrice = 111,
+            DepositPaid = true,
+            BookinDates = new BookingDates()
+            {
+                CheckIn = DateTime.Now.AddDays(5).ToString("yyyy-MM-dd"),
+                CheckOut = DateTime.Now.AddDays(12).ToString("yyyy-MM-dd")
+            },
+            AdditionalNeeds = "Breakfast"
+        };
+
+        public static BookingModel InValidBookingModel = new BookingModel()
+        {
+            BookinDates = new BookingDates()
+            {
+                CheckIn = DateTime.Now.AddDays(5).ToString("yyyy-MM-dd"),
+                CheckOut = DateTime.Now.AddDays(12).ToString("yyyy-MM-dd")
+            },
+            AdditionalNeeds = "Breakfast"
+        };
+    }
+}
